84e0e849a4c85a572f5820262a6b810c3dceb040
likwotsing <gq_li3051@163.com>
2020-08-28 17:36:01 +0800

    实现路由功能

---
+++ app.js
@@ -0,0 +1,68 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+
+var compression = require('compression');
+var helmet = require('helmet');
+
+var app = express();
+
+
+// Set up mongoose connection
+var mongoose = require('mongoose');
+var dev_db_url = 'mongodb+srv://cooluser:coolpassword@mycluster.7cxeu.azure.mongodb.net/local_library?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// // 设置 Mongoose 连接
+// const mongoose = require('mongoose');
+// const mongoDB = '在此插入数据库_URL';
+// mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+// mongoose.Promise = global.Promise;
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB 连接错误：'));
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(helmet());
+app.use(compression()); // Compress all routes
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ directoryList.md
@@ -0,0 +1,18 @@
+|-- undefined
+    |-- app.js
+    |-- package-lock.json
+    |-- package.json
+    |-- bin
+    |   |-- www
+    |-- public
+    |   |-- images
+    |   |-- javascripts
+    |   |-- stylesheets
+    |       |-- style.css
+    |-- routes
+    |   |-- index.js
+    |   |-- users.js
+    |-- views
+        |-- error.pug
+        |-- index.pug
+        |-- layout.pug
